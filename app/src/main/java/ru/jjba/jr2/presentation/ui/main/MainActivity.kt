fe1694997e2bf63a5049cf6fddc3b21f26b8ba92
Metlic <whitebox459steam@gmail.com>
2018-08-11 09:05:23 +0700

    project initial commit

---
+++ app/src/main/java/ru/jjba/jr2/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.jjba.jr2.presentation.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.jjba.jr2.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
