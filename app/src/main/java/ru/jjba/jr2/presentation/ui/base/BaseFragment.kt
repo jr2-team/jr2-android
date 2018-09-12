8744c90af45b3db6ea1238afbd46b58461421fe6
Metlic <whitebox459steam@gmail.com>
2018-09-12 13:47:47 +0700

    рефактор tts

--- app/src/main/java/ru/jjba/jr2/presentation/ui/base/BaseFragment.kt
+++ app/src/main/java/ru/jjba/jr2/presentation/ui/base/BaseFragment.kt
@@ -1,16 +1,21 @@
 package ru.jjba.jr2.presentation.ui.base
 
+import android.os.Build
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.support.design.widget.Snackbar
 import android.view.*
 import com.arellomobile.mvp.MvpAppCompatFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.jjba.jr2.App
 import ru.jjba.jr2.R
 import ru.jjba.jr2.utils.BottomNavigationItem
 import ru.jjba.jr2.utils.inflate
 import ru.jjba.jr2.utils.isVisible
 
 abstract class BaseFragment : MvpAppCompatFragment() {
+
+    private val textToSpeech: TextToSpeech = App.instance.tts
 
     abstract val layoutRes: Int
     abstract val titleDefault: String
@@ -24,14 +29,9 @@ abstract class BaseFragment : MvpAppCompatFragment() {
                 setHasOptionsMenu(true)
             }
 
-    open fun showMessage(msg: String) {
-        val view = view ?: return
-        Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show()
-    }
-
-    open fun showMessage(resMsg: Int) {
-        val view = view ?: return
-        Snackbar.make(view, resMsg, Snackbar.LENGTH_SHORT).show()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        isBnMainShown()
     }
 
     override fun onResume() {
@@ -46,9 +46,14 @@ abstract class BaseFragment : MvpAppCompatFragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        isBnMainShown()
+    open fun showMessage(msg: String) {
+        val view = view ?: return
+        Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show()
+    }
+
+    open fun showMessage(resMsg: Int) {
+        val view = view ?: return
+        Snackbar.make(view, resMsg, Snackbar.LENGTH_SHORT).show()
     }
 
     fun isBnMainShown(isShown: Boolean = true) {
@@ -58,4 +63,11 @@ abstract class BaseFragment : MvpAppCompatFragment() {
     fun setTitle(title: String) {
         activity?.title = title
     }
+
+    fun speakOut(text: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+        }
+    }
+
 }
--- app/src/main/java/ru/jjba/jr2/presentation/ui/word/details/WordDetailsFragment.kt
+++ app/src/main/java/ru/jjba/jr2/presentation/ui/word/details/WordDetailsFragment.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import kotlinx.android.synthetic.main.fragment_word_details.*
 import org.jetbrains.anko.bundleOf
+import ru.jjba.jr2.App
 import ru.jjba.jr2.R
 import ru.jjba.jr2.domain.entity.Interp
 import ru.jjba.jr2.domain.entity.Word
@@ -19,10 +19,8 @@ import ru.jjba.jr2.presentation.presenters.word.details.WordDetailsPresenter
 import ru.jjba.jr2.presentation.presenters.word.details.WordDetailsView
 import ru.jjba.jr2.presentation.ui.base.BaseFragment
 import ru.jjba.jr2.presentation.ui.interp.InterpAdapter
-import java.util.*
 
-class WordDetailsFragment : BaseFragment(), WordDetailsView, TextToSpeech.OnInitListener {
-
+class WordDetailsFragment : BaseFragment(), WordDetailsView {
     override val layoutRes: Int = R.layout.fragment_word_details
     override val titleDefault: String
         get() = getString(R.string.word_details_title)
@@ -34,8 +32,6 @@ class WordDetailsFragment : BaseFragment(), WordDetailsView, TextToSpeech.OnInit
         get() = arguments?.getLong(WORD_ID)
     private val interpAdapter = InterpAdapter()
 
-    private lateinit var textToSpeech: TextToSpeech
-
     @ProvidePresenter
     fun provideWordDetailsPresenter(): WordDetailsPresenter =
             WordDetailsPresenter(wordId)
@@ -45,44 +41,17 @@ class WordDetailsFragment : BaseFragment(), WordDetailsView, TextToSpeech.OnInit
 
         isBnMainShown(false)
 
-        textToSpeech = TextToSpeech(context, this)
-
         initContent()
     }
 
-    //TODO : Сделать рефактор кода с textToSpeech, перенести в App.kt?
-    override fun onInit(status: Int) {
-        if (status == TextToSpeech.SUCCESS) {
-            val result = textToSpeech.setLanguage(Locale.JAPANESE)
-
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS","The Language specified is not supported!")
-            } else {
-                tvWordDetailsJp.isEnabled = true
-            }
-
-        } else {
-            Log.e("TTS", "Initilization Failed!")
-        }
-    }
-
-    private fun speakOut(view: TextView) {
-        val text = view.text.toString()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            textToSpeech!!.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
-        }
-    }
-
     private fun initContent() {
-        rvInterp.also {
-            it.setHasFixedSize(true)
-            it.layoutManager = LinearLayoutManager(context)
-            it.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
-            it.adapter = interpAdapter
-        }
+        rvInterp.setHasFixedSize(true)
+        rvInterp.layoutManager = LinearLayoutManager(context)
+        rvInterp.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+        rvInterp.adapter = interpAdapter
 
-        tvWordDetailsJp.setOnClickListener { speakOut(it as TextView) }
-        tvWorddetailsFurigana.setOnClickListener { speakOut(it as TextView) }
+        tvWordDetailsJp.setOnClickListener { speakOut((it as TextView).text.toString()) }
+        tvWorddetailsFurigana.setOnClickListener { speakOut((it as TextView).text.toString()) }
     }
 
     override fun showWord(word: Word) {
