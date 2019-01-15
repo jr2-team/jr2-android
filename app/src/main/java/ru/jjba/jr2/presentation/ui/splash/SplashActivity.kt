84cf186a2119d73683a70846d98b9933082cceb4
PNZeml <pavelz1n@yandex.ru>
2019-01-15 13:09:58 +0700

    Fixes

--- app/src/main/java/ru/jjba/jr2/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/ru/jjba/jr2/presentation/ui/splash/SplashActivity.kt
@@ -3,38 +3,33 @@ package ru.jjba.jr2.presentation.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.addTo
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.rxkotlin.toObservable
-import io.reactivex.schedulers.Schedulers
+import androidx.lifecycle.*
 import ru.jjba.jr2.R
 import ru.jjba.jr2.presentation.ui.main.MainActivity
-import java.util.concurrent.TimeUnit
+import ru.jjba.jr2.presentation.viewmodel.splash.SplashViewModel
 
-class SplashActivity : AppCompatActivity() {
-    val d = CompositeDisposable()
+class SplashActivity : AppCompatActivity(), LifecycleObserver {
+    lateinit var viewModel: SplashViewModel
 
     // TODO: Добавить бэкграунд (https://plus.google.com/+AndroidDevelopers/posts/Z1Wwainpjhd)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_splash)
 
-        emptyList<Unit>().toObservable()
-                .delay(2, TimeUnit.SECONDS)
-                .doOnComplete {
-                    navToMainScreen()
-                }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy()
-                .addTo(d)
+        viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
+        viewModel.onSetupDb()
+
+        lifecycle.addObserver(this)
     }
 
-    private fun navToMainScreen() {
-        intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    private fun observeData() {
+        viewModel.observeIsAllowedNavToMain().observe(this, Observer { isAllowedToNav ->
+            if (isAllowedToNav) {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
     }
 }
